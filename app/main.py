3564d455e6f7ebeb3ef047aa817cf382de6a7cd8
Chiara <chiaravisca@outlook.com>
2025-04-09 16:04:25 +0200

    util change in main

--- app/main.py
+++ app/main.py
@@ -9,4 +9,4 @@ app.include_router(auth_router)
 
 @app.get("/")
 def root():
-    return {"message": "Adaptive MFA System API running."}
+    return {"message": "Adaptive MFA System API running. Check http://127.0.0.1:8000/docs or http://127.0.0.1:8000/redoc for available endpoints."}
