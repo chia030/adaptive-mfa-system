d46e15276b9e5995e612a1f9b69c91702ad13fb3
Chiara <chiaravisca@outlook.com>
2025-04-11 18:14:24 +0200

    add trusted devices routes to view and delete devices

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 from app.api.auth_routes import router as auth_router
 from app.api.mfa_routes import router as mfa_router
+from app.api.trusted_routes import router as trusted_router
 
 # Run with uvicorn app.main:app --reload
 # should it be running with docker-compose?
 
 app = FastAPI()
+
+# ROUTES:
 app.include_router(auth_router) # AUTH
 app.include_router(mfa_router) # MFA
+app.include_router(trusted_router) # TRUSTED DEVICES (list and revocation)
 
 @app.get("/")
 def root():
